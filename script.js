b74e8fd631af080d00a1110c897171899a4857f4
Bhavik <31174872+inhumanxd@users.noreply.github.com>
2023-03-19 17:39:07 +0530

    Added code to copy hashtags  based on button.

--- script.js
+++ script.js
@@ -1,61 +1,23 @@
-const copyButton1 = document.getElementById("copyButton1");
-const copyButton2 = document.getElementById("copyButton2");
-const copyButton3 = document.getElementById("copyButton3");
-const copyButton4 = document.getElementById("copyButton4");
-const copyButton5 = document.getElementById("copyButton5");
-const copyButton6 = document.getElementById("copyButton6");
-const copyButton7 = document.getElementById("copyButton7");
-const copyButton8 = document.getElementById("copyButton8");
-const copyButton9 = document.getElementById("copyButton9");
-const copyButton10 = document.getElementById("copyButton10");
-const output = document.getElementById("output");
-    
-    copyButton1.addEventListener("click", () => {
-      navigator.clipboard.writeText("Text 1");
-      output.innerText = "Text 1 copied to clipboard";
-    });
-    
-    copyButton2.addEventListener("click", () => {
-      navigator.clipboard.writeText("Text 2");
-      output.innerText = "Text 2 copied to clipboard";
-    });
-    
-    copyButton3.addEventListener("click", () => {
-      navigator.clipboard.writeText("Text 3");
-      output.innerText = "Text 3 copied to clipboard";
-    });
-    
-    copyButton4.addEventListener("click", () => {
-      navigator.clipboard.writeText("Text 4");
-      output.innerText = "Text 4 copied to clipboard";
-    });
-    
-    copyButton5.addEventListener("click", () => {
-      navigator.clipboard.writeText("Text 5");
-      output.innerText = "Text 5 copied to clipboard";
-    });
-    
-    copyButton6.addEventListener("click", () => {
-      navigator.clipboard.writeText("Text 6");
-      output.innerText = "Text 6 copied to clipboard";
-    });
-    
-    copyButton7.addEventListener("click", () => {
-      navigator.clipboard.writeText("Text 7");
-      output.innerText = "Text 7 copied to clipboard";
-    });
-    
-    copyButton8.addEventListener("click", () => {
-      navigator.clipboard.writeText("Text 8");
-      output.innerText = "Text 8 copied to clipboard";
-    });
+const timeout = 2000;
+const buttons = Array.from(document.querySelectorAll(".copyButton"));
 
-     copyButton9.addEventListener("click", () => {
-      navigator.clipboard.writeText("Text 9");
-      output.innerText = "Text 9 copied to clipboard";
-    });
+const hashtags = {
+  "RED BULL RACING": ["values1", "values2", "values3"],
+  FERRARI: ["values4", "values5", "values6"],
+  MERCEDES: ["values7", "values8", "values9"],
+  ALPINE: ["values10", "values11", "values12"],
+};
 
-      copyButton10.addEventListener("click", () => {
-        navigator.clipboard.writeText("Text 10");
-        output.innerText = "Text 10 copied to clipboard";
-    });
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    const btnText = button.innerText;
+
+    navigator.clipboard.writeText(hashtags[btnText]);
+
+    button.innerText = "Copied to clipboard 📋";
+
+    setTimeout(() => {
+      button.innerText = btnText;
+    }, timeout);
+  })
+);
