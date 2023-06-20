b5f5ef7bcd3738ee1b2e4a9945dc39db6511cca9
typeJay <thatjayc@gmail.com>
2023-06-20 20:16:43 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   index.html
    	modified:   script.js
    	modified:   style.css

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ const timeout = 2000;
 const buttons = Array.from(document.querySelectorAll(".copyButton"));
 
 const hashtags = {
-  "RED BULL RACING": ["values1", "values2", "values3"],
-  FERRARI: ["values4", "values5", "values6"],
-  MERCEDES: ["values7", "values8", "values9"],
-  ALPINE: ["values10", "values11", "values12"],
-  MCLAREN: ["values10", "values11", "values12"],
-  "ALFA ROMEO": ["values10", "values11", "values12"],
-  "ASTON MARTIN": ["values10", "values11", "values12"],
-  HAAS: ["values10", "values11", "values12"],
-  ALPHATAURI: ["values10", "values11", "values12"],
+  "RED BULL RACING":
+    "#RedBull #RB19 #RB18 #RedBullPowertrains #MaxVerstappen #SergioPerez #verstappen33 #redbull #mv33 #SergioPerez #SergioPerez11 #Perez11 #formulanerds #f1podcast #racingpodcast #podcast #f1 #formula1 #racing #motorsport #raceweek #formulaone #motorsport #redbullracing #max33",
+
+  FERRARI:
+    "#scuderiaferrari #Ferrari #SF23 #F175 #Ferrari #CharlesLeclerc #CarlosSainz #ferrarimotorsport #ferrarif1 #scuderiaferrarif1 #formula1 #formulaone #motorsport #grandprix #scuderia #cs55 #charlesleclerc #charlesleclerc16 #leclerc #leclerc16 #charles16 #cl16 #formulanerds",
+
+  MERCEDES:
+    "#Mercedes #Mercedesamgf1 #W14 #W13 #Mercedes #LewisHamilton #teamLH44 #LH44 #LewisHamilton44 #Lewis44 #Hamilton44 #team44 #GeorgeRussell #GeorgeRussell63 #mercedesf1 #mercedesamgf1team #mercedesamgf1petronas #toto #formulanerds #f1podcast #racingpodcast #podcast #racing #formulaone #motorsport",
+
+  ALPINE: "values10 values11 values12",
+
+  MCLAREN: "values10 values11 values12",
+
+  "ALFA ROMEO": "values10 values11 values12",
+
+  "ASTON MARTIN":
+    "#AstonMartin #AMR23 #AMR22 #Mercedes #FernandoAlonso #LanceStroll #SebastianVettel #astonmartinf1 #f1 #formula1 #formulaone #racing #motorsport #alonso #LanceStroll #grandprix #astonmartinf1team #astonmartinracing #fa14 #alonso14 #ls18 #motorsports #formulanerds #f1podcast #racingpodcast #raceweek",
+
+  HAAS: "values10 values11 values12",
+
+  ALPHATAURI: "values10 values11 values12",
+
   WILLIAMS: "values10 values11 values12",
 };
 
